5cbf3e897c0df2d43468a1d8745af459b2c0b573
Ibrahim <ibrahimmuhammad018@gmail.com>
2022-06-29 05:52:18 +0000

    Update Polls model and make new migrations

--- polls/models.py
+++ polls/models.py
@@ -5,11 +5,19 @@ from django.db import models
 
 class Question(models.Model):  # id = models.PrimaryKey(auto_increment=True)
     question_text = models.CharField(max_length=255)
+    question_description = models.CharField(max_length=255, default="Basic description")
     pub_date = models.DateField(auto_now_add=True)
 
+    def __str__(self):
+        return self.question_text
 
-class Choices(models.Model):                                   # id = models.PrimaryKey(auto_increment=True) -Django already does it automatically. that's why it is commented out.
+
+class Choices(
+    models.Model
+):  # id = models.PrimaryKey(auto_increment=True) -Django already does it automatically. that's why it is commented out.
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_answer = models.CharField(max_length=20)
     votes = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.choice_answer
